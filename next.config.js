b7f6bfa348c6f3ebf4fb98e6dbb35afb6cfa08f3
DanielCG <danielcgilibert@gmail.com>
2023-03-16 22:09:35 +0100

    🔧  chore: add remove console

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
   images: {
     remotePatterns: [
       {
