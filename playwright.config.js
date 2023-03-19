daf0421bf1a190a44ea422486c0146b06352ac40
DanielCG <danielcgilibert@gmail.com>
2023-03-19 16:11:05 +0100

    🔧 chore: webSever

--- playwright.config.js
+++ playwright.config.js
@@ -84,8 +84,8 @@ module.exports = defineConfig({
   // outputDir: 'test-results/',
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
+  webServer: {
+    command: 'pnpm run start',
+    port: 3000,
+  },
 })
