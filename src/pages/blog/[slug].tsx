29fa155f19b266be089f7e8a99893d86e2fae19c
DanielCG <danielcgilibert@gmail.com>
2023-03-23 10:50:57 +0100

    🔍 chore: upgrade seo blog page

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -6,6 +6,7 @@ import { api } from '@/utils/api'
 import { NextPage } from 'next'
 import { MDXRemote } from 'next-mdx-remote'
 import { serialize } from 'next-mdx-remote/serialize'
+import { NextSeo } from 'next-seo'
 import { useRouter } from 'next/router'
 import { useCallback, useRef } from 'react'
 import ReactCanvasConfetti from 'react-canvas-confetti'
@@ -49,49 +50,52 @@ const PostPage: NextPage<PostProps> = ({ post: { attributes }, mdxSource }) => {
     })
   }, [makeShot])
   return (
-    <div className='px-8 md:p-0'>
-      <header className=' mb-8 flex items-center justify-between'>
-        <Button
-          className='flex items-center justify-center px-4 text-sm font-light'
-          onClick={() => router.back()}
-        >
-          <IoIosArrowBack size={20} />
-          Atras
-        </Button>
-
-        <div className='relative flex items-center justify-end gap-4 border-customGray pb-2 md:w-fit md:border-b-2 '>
-          <ReactCanvasConfetti
-            refConfetti={getInstance}
-            style={{
-              position: 'absolute',
-              pointerEvents: 'none',
-              width: '100%',
-              height: '100%',
-              top: 0,
-              left: 0
-            }}
-          />
-          <span className='hidden text-sm font-extralight opacity-60 md:block'>
-            Tiempo 5 min
-          </span>
+    <>
+      <NextSeo title={`${name}`} description={`post sobre ${name}`} />
+      <div className='px-8 md:p-0'>
+        <header className=' mb-8 flex items-center justify-between'>
           <Button
-            onClick={() => {
-              navigator.clipboard.writeText(window.location.href)
-              fire()
-            }}
-            className=' flex items-center justify-center gap-1 text-sm font-light'
+            className='flex items-center justify-center px-4 text-sm font-light'
+            onClick={() => router.back()}
           >
-            <AiOutlineShareAlt size={20} /> Share
+            <IoIosArrowBack size={20} />
+            Atras
           </Button>
-        </div>
-      </header>
 
-      <article className='prose'>
-        <h1 className='m-0 text-lightOrange'>{name}</h1>
-        <span className='text-darkOrange'>{formatDate}</span>
-        <MDXRemote {...mdxSource} components={components} />
-      </article>
-    </div>
+          <div className='relative flex items-center justify-end gap-4 border-customGray pb-2 md:w-fit md:border-b-2 '>
+            <ReactCanvasConfetti
+              refConfetti={getInstance}
+              style={{
+                position: 'absolute',
+                pointerEvents: 'none',
+                width: '100%',
+                height: '100%',
+                top: 0,
+                left: 0
+              }}
+            />
+            <span className='hidden text-sm font-extralight opacity-60 md:block'>
+              Tiempo 5 min
+            </span>
+            <Button
+              onClick={() => {
+                navigator.clipboard.writeText(window.location.href)
+                fire()
+              }}
+              className=' flex items-center justify-center gap-1 text-sm font-light'
+            >
+              <AiOutlineShareAlt size={20} /> Share
+            </Button>
+          </div>
+        </header>
+
+        <article className='prose mx-auto w-full'>
+          <h1 className='m-0 text-lightOrange'>{name}</h1>
+          <span className='text-darkOrange'>{formatDate}</span>
+          <MDXRemote {...mdxSource} components={components} />
+        </article>
+      </div>
+    </>
   )
 }
 
