76e9e412eb6683a430349ec72b43f96f0f1d8c0e
DanielCG <danielcgilibert@gmail.com>
2023-03-29 22:57:45 +0200

    ✨ feat: bot questions

---
+++ src/data/questions.ts
@@ -0,0 +1,55 @@
+export const QUESTIONS = [
+  { text: '¿Donde puedo contactarte?', label: 'contacto' },
+  { text: '¿Tienes redes sociales?', label: 'contacto' },
+  { text: 'Dime tus redes sociales', label: 'contacto' },
+  { text: '¿De donde eres?', label: 'intro' },
+  { text: '¿Tienes trabajo?', label: 'intro' },
+  { text: '¿Donde trabajas?', label: 'intro' },
+  { text: '¿Cuantos años de experiencia tienes?', label: 'intro' },
+  { text: '¿Cuál es tu puesto actual?', label: 'intro' },
+  { label: 'contacto', text: '¿Donde puedo contactarte?' },
+  { label: 'contacto', text: '¿Tienes redes sociales?' },
+  { label: 'contacto', text: 'Dime tus redes sociales' },
+  { label: 'intro', text: '¿De donde eres?' },
+  { label: 'intro', text: '¿Tienes trabajo?' },
+  { label: 'intro', text: '¿Donde trabajas?' },
+  { label: 'intro', text: '¿Cuantos años de experiencia tienes?' },
+  { label: 'intro', text: '¿Cuál es tu puesto actual?' },
+  { label: 'contacto', text: '¿Donde puedo contactarte?' },
+  { label: 'intro', text: '¿Qué idiomas hablas?' },
+  {
+    label: 'teg',
+    text: '¿En qué lenguajes de programación tienes experiencia?'
+  },
+  {
+    label: 'teg',
+    text: '¿Tienes experiencia en programación orientada a objetos?'
+  },
+  {
+    label: 'teg',
+    text: '¿Has trabajado con sistemas de control de versiones? Si es así, ¿cuál prefieres?'
+  },
+  { label: 'teg', text: '¿Cuál es tu herramienta de programación favorita?' },
+  {
+    label: 'teg',
+    text: '¿Tienes experiencia en trabajo remoto o distribuido?'
+  },
+  {
+    label: 'teg',
+    text: '¿Cómo te mantienes actualizado/a con las últimas tecnologías y tendencias en programación?'
+  },
+  {
+    label: 'teg',
+    text: '¿Cuál es tu framework preferido para el desarrollo de aplicaciones web?'
+  },
+  { label: 'teg', text: '¿Que tecnologías sabes?' },
+  { label: 'teg', text: '¿Que tecnologías usas?' },
+  {
+    label: 'teg',
+    text: '¿Tienes experiencia en el desarrollo de aplicaciones de escritorio?'
+  },
+  { label: 'contacto', text: '¿Dónde puedo contactar contigo?\n' },
+  { label: 'contacto', text: 'Dime tu correo' },
+  { label: 'contacto', text: 'Contacto' },
+  { label: 'contacto', text: '¿Cómo hablo contigo?' }
+]
