8836b967da76d97d28b55fdb62062348b4b3ec63
DanielCG <danielcgilibert@gmail.com>
2023-03-29 22:57:25 +0200

    ✨ feat: data bot

---
+++ src/data/answers.tsx
@@ -0,0 +1,16 @@
+export const ANSWERS = {
+  default: <p>I'm sorry, I don't understand your question.</p>,
+  intro: <p></p>,
+  contacto: (
+    <p>
+      Puedes contactarme a través de mi correo electrónico:
+      <br />
+      <a
+        className=' font-bold underline'
+        href='mailto:danielcgilibert@gmail.com'
+      >
+        Contacto
+      </a>
+    </p>
+  )
+}
