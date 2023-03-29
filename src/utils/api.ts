f135d6739dac9fe3399c47683d571af100b65577
DanielCG <danielcgilibert@gmail.com>
2023-03-29 22:58:35 +0200

    ✨ feat: add bot api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const api = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
-  timeout: 1000,
+  timeout: 5000,
   headers: {
     Authorization: `Bearer ${process.env.ENV_TOKEN_API}`
   }
@@ -10,7 +10,7 @@ export const api = axios.create({
 
 export const apiNotion = axios.create({
   baseURL: `https://api.notion.com/v1`,
-  timeout: 1000,
+  timeout: 5000,
   headers: {
     Authorization: `Bearer ${process.env.ENV_TOKEN_NOTION}`
   }
@@ -18,7 +18,7 @@ export const apiNotion = axios.create({
 
 export const apiGitHub = axios.create({
   baseURL: `https://api.github.com/users/danielcgilibert`,
-  timeout: 1000,
+  timeout: 5000,
   withCredentials: true,
   headers: {
     Authorization: `Bearer ${process.env.ENV_GITHUBTOKEN}`,
@@ -26,7 +26,16 @@ export const apiGitHub = axios.create({
   }
 })
 
+export const apiBot = axios.create({
+  baseURL: `https://api.cohere.ai/v1/classify`,
+  timeout: 10000,
+  headers: {
+    Authorization: `Bearer BnYyKrmBAhZrvCKSujwnTbAoiTfBDt8JXV2au8Yc`,
+    'Content-Type': 'application/json'
+  }
+})
+
 export const apiContribu = axios.create({
   baseURL: `https://github-contributions-api.deno.dev`,
-  timeout: 1000
+  timeout: 5000
 })
