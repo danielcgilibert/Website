3d5102d66e8dd4539df61d2993ca5246451858ac
DanielCG <danielcgilibert@gmail.com>
2023-03-29 22:56:14 +0200

    ✨ feat: add bot

--- src/components/aboutMeSection.tsx
+++ src/components/aboutMeSection.tsx
@@ -1,14 +1,15 @@
 import { ExternalLink } from '@/ui/link'
 import { AiFillLinkedin } from 'react-icons/ai'
 import { BsGithub } from 'react-icons/bs'
+import Bot from './bot'
 
 const AboutMeSection = () => {
   return (
-    <section className='flex flex-col gap-5  md:pr-24'>
+    <section className=' flex flex-col gap-5  md:pr-24'>
       <header>
         <h1 className='text-4xl font-bold md:text-5xl '>Daniel Carmona</h1>
-        <h2 className='text-sm tracking-widest text-lightOrange antialiased md:text-xl  '>
-          FRONT-END DEVELOPER
+        <h2 className='flex gap-2 text-sm tracking-widest text-lightOrange antialiased md:text-xl  '>
+          FRONT-END DEVELOPER <Bot />
         </h2>
         <div className='mt-3 flex gap-2 text-white text-opacity-50 '>
           <ExternalLink href='https://github.com/danielcgilibert'>
