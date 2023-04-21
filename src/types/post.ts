d66144a42817d9dfb026d7d17a9200d05867ac28
DanielCG <danielcgilibert@gmail.com>
2023-04-21 13:03:01 +0200

    Add images and publishedAt fields to PostAttributes interface and create supporting interfaces for Images, Datum, Attributes, Formats, and ProviderMetadata

--- src/types/post.ts
+++ src/types/post.ts
@@ -9,6 +9,8 @@ export interface PostAttributes {
   icon: string
   description: string
   category: Category
+  images: Images
+  publishedAt: Date
 }
 
 export interface Category {
@@ -25,4 +27,57 @@ export interface DataAttributes {
   createdAt: Date
   updatedAt: Date
   publishedAt: Date
+}
+
+export interface Images {
+  data: Datum
+}
+
+export interface Datum {
+  id: number
+  attributes: Attributes
+}
+
+export interface Attributes {
+  name: string
+  alternativeText: null
+  caption: null
+  width: number
+  height: number
+  formats: Formats
+  hash: string
+  ext: string
+  mime: string
+  size: number
+  url: string
+  previewUrl: null
+  provider: string
+  provider_metadata: ProviderMetadata
+  createdAt: Date
+  updatedAt: Date
+}
+
+export interface Formats {
+  large: Large
+  small: Large
+  medium: Large
+  thumbnail: Large
+}
+
+export interface Large {
+  ext: string
+  url: string
+  hash: string
+  mime: string
+  name: string
+  path: null
+  size: number
+  width: number
+  height: number
+  provider_metadata: ProviderMetadata
+}
+
+export interface ProviderMetadata {
+  public_id: string
+  resource_type: string
 }
