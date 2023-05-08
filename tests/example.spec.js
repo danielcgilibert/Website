b9170fb1f4182d1fa52999846cc2a9affa97140f
DanielCG <danielcgilibert@gmail.com>
2023-05-08 20:03:12 +0200

    🐛 fix(playwright.config.ts): change baseURL to use 127.0.0.1 instead of localhost to avoid DNS resolution issues ✨ feat(playwright.config.ts): add reuseExistingServer option to webServer configuration to avoid starting a new server instance on each test run 🐛 fix(playwright.config.ts): change port to url option in webServer configuration to avoid port conflicts with other services 🐛 fix(example.spec.js): change page.goto() to use relative path instead of absolute URL to avoid issues with baseURL configuration in playwright.config.ts

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,7 +2,7 @@
 import { expect, test } from '@playwright/test'
 
 test('has title', async ({ page }) => {
-  await page.goto('http://localhost:3000')
+  await page.goto('/')
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Daniel Carmona - Desarrollador Front-End/)
