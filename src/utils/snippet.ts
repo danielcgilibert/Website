ab0aa90a2c3eaa1e07f5e5f3f953f421dfeba221
Daniel <danielcgilibert@gmail.com>
2023-11-03 23:52:01 +0100

    new design

--- src/utils/snippet.ts
+++ src/utils/snippet.ts
@@ -1,7 +1,5 @@
 import { getCollection } from 'astro:content'
 
 export const getSnippets = async (max?: number) => {
-  return (await getCollection('snippet'))
-    .filter((snippet) => !snippet.data.draft)
-    .slice(0, max)
+	return (await getCollection('snippets')).filter((snippet) => !snippet.data.draft).slice(0, max)
 }
